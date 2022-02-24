484ec73101c3f6caca46e0c978c034b81a95161c
Pratik Deshmukh <pratikdeshmukhlobhi@gmail.com>
2022-02-24 12:06:12 +0530

    reset password with new template

--- services/user.js
+++ services/user.js
@@ -1,6 +1,9 @@
 const User = require("../models/user");
 const bcrypt = require("bcrypt");
 const nodemailer = require("nodemailer");
+const fs = require("fs");
+const path = require("path");
+const mustache = require("mustache");
 
 module.exports = class UserService {
   async findAll(txn) {
@@ -52,6 +55,15 @@ module.exports = class UserService {
     return await User.query().update(updateCreadencials).where("id", id);
   }
 
+  renderHtmlTemplate(filename, variables) {
+    const content = fs.readFileSync(
+      path.join(__dirname, "../static/templates/" + filename),
+      "utf-8"
+    );
+    var output = mustache.render(content, variables);
+    return output;
+  }
+
   async sendEmail(user, token) {
     try {
       const transporter = nodemailer.createTransport({
@@ -64,15 +76,14 @@ module.exports = class UserService {
           pass: process.env.MAIL_PASS,
         },
       });
-
       await transporter.sendMail({
         from: process.env.EMAIL_USER,
         to: user.email,
         subject: "Reset Password Link - blogofile.herokuapp.com",
-        html:
-          '<p>You requested for reset password, kindly use this <a href="http://localhost:2022/reset-password/' +
-          token +
-          '">link</a> to reset your password</p>',
+        html: await this.renderHtmlTemplate("forgot.html", {
+          user_name: user.name,
+          token: process.env.BASE_URL+"/reset-password/"+token,
+        }),
       });
 
       console.log("email sent sucessfully");
